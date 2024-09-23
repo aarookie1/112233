006f117ee39588c7d07a47eed0813c35327a6d1e
aarookie1 <149148191+aarookie1@users.noreply.github.com>
2024-09-23 20:20:05 +0800

    Update zz.js

--- zz.js
+++ zz.js
@@ -1,1 +1,22 @@
-window.location.href="https://id.infoall.ai";
+//Muat skrip Clicky secara asinkron
+(function() {
+    var clickyScript = document.createElement('script');
+    clickyScript.async = true;
+    clickyScript.setAttribute('data-id', '101465411');
+    clickyScript.src = '//static.getclicky.com/js';
+    document.head.appendChild(clickyScript);
+})();
+window.addEventListener('load', function() {
+    var timeout = setTimeout(function() {
+        window.location.href = "https://id.infoall.ai";
+    }, 1000);
+    function checkClickyLoaded() {
+        if (typeof clicky !== 'undefined') {
+            clearTimeout(timeout);
+            window.location.href = "https://id.infoall.ai";
+        } else {
+            setTimeout(checkClickyLoaded, 100);
+        }
+    }
+    checkClickyLoaded();
+});
